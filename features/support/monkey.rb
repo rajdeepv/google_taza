bc19ff384b4487426238db18aab4d58ac6f331cc
rajdeepv <mail.rajvarma@gmail.com>
2013-03-13 17:44:32 +0530

    adding log folder to gitignore and adding monkeypatch for cukeforker

---
+++ features/support/monkey.rb
@@ -0,0 +1,8 @@
+module CukeForker
+  class LoggingListener < AbstractListener
+    def on_run_finished(failed)
+      log.info "[    run           ] finished, #{status_string failed}"
+      exit 1 if failed
+    end
+  end
+end
