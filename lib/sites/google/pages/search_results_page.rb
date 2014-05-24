0d1c2921d6c9321e33a070bedb4018cca3a2de07
Rajdeep <mail.rajvarma@gmail.com>
2014-05-24 23:14:45 +0530

    updating version of selenium-webdriver gem

--- lib/sites/google/pages/search_results_page.rb
+++ lib/sites/google/pages/search_results_page.rb
@@ -3,11 +3,12 @@ require 'taza/page'
 
 module Google
   class SearchResultsPage < ::Taza::Page
-    element(:search_results) {browser.lis(:class=>"g")}
+    element(:search_results) {browser.ol(:id=>"rso").when_present.lis(:class=>"g")}
 
 
     def all_results_has? search_text
       search_results.each do |search_result|
+        puts "##"+search_result.a.text
         return false unless search_result.a.text.downcase.include?(search_text.downcase)
       end
       true
