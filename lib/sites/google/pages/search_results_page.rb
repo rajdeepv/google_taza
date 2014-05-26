6211fd526a2e3423773f005123dc1026fc829755
Rajdeep <mail.rajvarma@gmail.com>
2014-05-26 09:50:13 +0530

    test checkin for jenkins

--- lib/sites/google/pages/search_results_page.rb
+++ lib/sites/google/pages/search_results_page.rb
@@ -8,7 +8,7 @@ module Google
 
     def all_results_has? search_text
       search_results.each do |search_result|
-        puts "##"+search_result.a.text
+        puts "#"+search_result.a.text
         return false unless search_result.a.text.downcase.include?(search_text.downcase)
       end
       true
